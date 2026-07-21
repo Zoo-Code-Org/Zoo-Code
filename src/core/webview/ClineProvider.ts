ae21d239960ee3131faab247f6daf4bc39ca964e
Eason Liang <easonliang28@gmail.com>
2026-07-22 02:51:45 +0800

    fix(provider): sync view-local state when activating provider profile

--- src/core/webview/ClineProvider.ts
+++ src/core/webview/ClineProvider.ts
@@ -2060,13 +2060,9 @@ export class ClineProvider
 						this.updateGlobalState("currentApiConfigName", name),
 						this.providerSettingsManager.setModeConfig(mode, id),
 						this.contextProxy.setProviderSettings(providerSettings),
+						this.saveViewState("currentApiConfigName", name),
+						this.saveViewState("apiConfiguration", providerSettings),
 					])
-
-					this._updateViewLocalStateFromMutation({
-						listApiConfigMeta,
-						currentApiConfigName: name,
-						apiConfiguration: providerSettings,
-					})
 
 					// Change the provider for the current task.
 					// TODO: We should rename `buildApiHandler` for clarity (e.g. `getProviderClient`).
@@ -2191,13 +2187,9 @@ export class ClineProvider
 				this.contextProxy.setValue("listApiConfigMeta", listApiConfigMeta),
 				this.contextProxy.setValue("currentApiConfigName", name),
 				this.contextProxy.setProviderSettings(providerSettings),
+				this.saveViewState("currentApiConfigName", name),
+				this.saveViewState("apiConfiguration", providerSettings),
 			])
-
-			this._updateViewLocalStateFromMutation({
-				listApiConfigMeta,
-				currentApiConfigName: name,
-				apiConfiguration: providerSettings,
-			})
 		}
 
 		const { mode } = await this.getState()
