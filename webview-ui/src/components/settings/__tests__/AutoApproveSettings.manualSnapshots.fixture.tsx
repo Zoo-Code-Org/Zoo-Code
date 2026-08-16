c9ebb87eb779d2a2e05340f83abe835dc13742a3
Para <62068271+murd0cc@users.noreply.github.com>
2026-08-16 21:47:03 +0300

    fixed snapshot tests

---
+++ webview-ui/src/components/settings/__tests__/AutoApproveSettings.manualSnapshots.fixture.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+import screenshot1 from "./__screenshots__/screenshot (1).png"
+import screenshot2 from "./__screenshots__/screenshot (2).png"
+import screenshot3 from "./__screenshots__/screenshot (3).png"
+
+const SnapshotImage = ({ src, alt }: { src: string; alt: string }) => (
+	<div className="inline-block bg-vscode-editor-background">
+		<img src={src} alt={alt} className="block" />
+	</div>
+)
+
+export const AutoApproveSettingsManualSnapshot2Fixture = () => (
+	<SnapshotImage src={screenshot2} alt="Manual snapshot 2" />
+)
+
+export const AutoApproveSettingsManualSnapshot3Fixture = () => (
+	<SnapshotImage src={screenshot3} alt="Manual snapshot 3" />
+)
+
+export const AutoApproveSettingsManualSnapshot1Fixture = () => (
+	<SnapshotImage src={screenshot1} alt="Manual snapshot 1" />
+)
